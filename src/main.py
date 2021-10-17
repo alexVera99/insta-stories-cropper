3a0938e33109296dbdb1ac73fd1f002939838f2d
alexanderjoelvera99 <alexanderjoel.vera99@gmail.com>
2021-10-17 18:59:27 +0200

    Save the video

--- src/main.py
+++ src/main.py
@@ -15,16 +15,23 @@ success, img = cap.read()
 # Capturing some frame information
 h_img, w_img, c_img = img.shape
 c_img = [int(w_img//2), int(h_img//2)]
+fps = cap.get(cv2.CAP_PROP_FPS)
 
 # Finding the size of the cropping given some ratio
 cropper = cropper.Cropper()
 cropper.findCropSize(ratio[0], ratio[1], w_img, h_img)
+print(int(cropper.width), int(cropper.height))
 
 # Time coherence corrector
 corrector = TimeCoherenceCorrector(c_img, th)
 
 # Initializing the face detector
 detector = faceDetector.faceDetector()
+
+# Create a video writer to save the resulting video
+video_writer = cv2.VideoWriter('videos/output/filename.avi',
+                                cv2.VideoWriter_fourcc(*'MJPG'),
+                                fps=fps, frameSize=(int(cropper.width), int(cropper.height)))
 
 while True:
     if(not success):
@@ -48,10 +55,15 @@ while True:
             
             # By the moment, only for the first face detected
             break
-
+    video_writer.write(img)
+    print(img.shape)
     cv2.imshow("Image", img)
     cv2.waitKey(1)
 
     # Read next frame
     success, img = cap.read()
 
+cap.release()
+video_writer.release()
+
+cv2.destroyAllWindows()
