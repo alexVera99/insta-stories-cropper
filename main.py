ba128d3ca97fd9c86c67402a806a08900131551a
alexanderjoelvera99 <alexanderjoel.vera99@gmail.com>
2021-10-17 13:05:52 +0200

    First approach of time coherence
    Here, compute the mean between the previous mean and the current one. However, the results are not quite good.
    This is because the new position has the half weight that the mean of the previous frame. Then, it changes a lot the mean value that will be get as center of the bbox.
    In order to improve, we are going to build a history array that will keep the values of all the previous position, and we are going to obtain the mean at each iteration using this. Also, we are going to compute the distance between the new frame bbox and the previous one. This will be done in the branc time-coherence-option-2

--- main.py
+++ main.py
@@ -1,9 +1,10 @@
 import FaceDetectionModule
 import cv2
 import CropModule
+import numpy as np
 
 # Capturing the first frame
-cap = cv2.VideoCapture("videos/music_video.mp4")
+cap = cv2.VideoCapture("videos/1.mp4")
 success, img = cap.read()
 # Capturing some frame information
 h_img, w_img, c_img = img.shape
@@ -16,6 +17,10 @@ cropper.findCropSize(ratio[0], ratio[1], w_img, h_img)
 
 # Initializing the face detector
 detector = FaceDetectionModule.faceDetector()
+
+# Time consistency parameters
+th = 2800
+mean_c_bboxs = np.array(c_img)
 
 while True:
     if(not success):
@@ -31,7 +36,21 @@ while True:
     else:
         # Only for the first detection
         for bbox in bboxs:
-            c_bbox = bbox[2]
+            cur_c_bbox = bbox[2]
+            cur_mean_c_bbox = [cur_c_bbox, mean_c_bboxs]
+            print("Current_pos: " + str(cur_c_bbox))
+            print("Mean: " + str(mean_c_bboxs))
+            dist = np.linalg.norm(cur_c_bbox, 2)
+
+            if(dist > th):
+                mean_c_bboxs = cur_c_bbox
+            else:
+                mean_c_bboxs = np.mean(cur_mean_c_bbox, axis=0)
+
+            print("Dist: " + str(dist))
+            #c_bbox = bbox[2]
+            c_bbox = list(map(int,mean_c_bboxs))
+            print("Box pos " + str(c_bbox) + "\n\n")
             img = cropper.crop(img, c_bbox)
             # By the moment, only for the first face detected
             break
